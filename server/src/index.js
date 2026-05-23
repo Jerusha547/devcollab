7f0b77917bce1f70f0116565e61f88d2395d13e9
marthajerushamarumudi7@gmail.com <144675544+Jerusha547@users.noreply.github.com>
2026-05-23 08:28:07 +0530

    fix: dynamic callback URL for production

--- server/src/index.js
+++ server/src/index.js
@@ -73,7 +73,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/github/callback",
+      callbackURL: `${process.env.SERVER_URL || "http://localhost:5000"}/auth/github/callback`,
     },
     (accessToken, refreshToken, profile, done) => {
       const user = {
